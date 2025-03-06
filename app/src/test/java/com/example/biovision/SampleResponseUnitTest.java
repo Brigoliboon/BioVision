1aeb72fb6202e01bea9d22bc7d024cb63c0017a8
brigoliboon <brigoliboonjefferson@gmail.com>
2025-03-06 20:15:18 +0800

    Added Template

---
+++ app/src/test/java/com/example/biovision/SampleResponseUnitTest.java
@@ -0,0 +1,21 @@
+package com.example.biovision;
+
+import org.junit.Test;
+
+public class SampleResponseUnitTest {
+
+    @Test
+    public void testSearchPlant(){
+
+    }
+
+    @Test
+    public  void testGetPlantInfo(){
+
+    }
+
+    @Test
+    public void testScanPlant(){
+
+    }
+}
