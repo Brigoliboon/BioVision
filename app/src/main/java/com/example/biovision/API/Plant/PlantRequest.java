5f307a7125670bbfffc453d7bd748283e079a69a
brigoliboon <brigoliboonjefferson@gmail.com>
2025-03-06 20:24:29 +0800

    Implemented plant search

--- app/src/main/java/com/example/biovision/API/Plant/PlantRequest.java
+++ app/src/main/java/com/example/biovision/API/Plant/PlantRequest.java
@@ -1,4 +1,28 @@
 package com.example.biovision.API.Plant;
 
+import com.example.biovision.API.Request.Request;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+
 public class PlantRequest {
+
+    private String api_key;
+    public PlantRequest(String api_key){
+        this.api_key = api_key;
+    }
+
+    public ResponseBody plantSearch(String q) throws IOException {
+        Request request = new Request(api_key, "https://bio-vision-api.vercel.app/api/v1/plant/search");
+
+        Map<String, String> params = Map.of(
+                "q", q
+        );
+
+        ResponseBody response = request.GET((HashMap<String, String>) params);
+        return  response;
+    }
 }
